75d10e57cf55ed45cc6a38aa1de1f1caeb8ceb24
Justin Brown <justin.brown@fandingo.org>
2011-12-05 06:38:25 -0600

    Partially Working
    Trying to fix a bug where iteration fails for some directories.

--- mkv-convert.sh
+++ mkv-convert.sh
@@ -1,45 +1,73 @@
 #!/bin/bash
 
-find . -type f | grep '.mkv$' | while read file; do
-    directory=`dirname "$file"`
-    title=`basename "$file" .mkv`
-    AC3=`mkvinfo "$file" | grep AC3` #check if it's AC3 audio or DTS
-    AAC=`mkvinfo "$file" | grep AAC`
-    order=`mkvinfo "$file" | grep "Track type" | sed 's/.*://' | head -n 1 | tr -d " "` #check if the video track is first or the audio track
-    if [ "$order" = "video" ]; then
-        fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | head -n 1` #store the fps of the video track
-	if [ -n "$AC3" ]; then
-	    mkvextract tracks "$file" 1:"${title}".264 2:"${title}".ac3 
-	    mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
-	    faac -o "${title}".aac "${title}".wav
-	elif [ -n "$AAC" ]; then
-	    mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
-	else
-	    mkvextract tracks "$file" 1:"${title}".264 2:"${title}".dts
-	    ffmpeg -i "${title}".dts -acodec libfaac -ab 576k "${title}".aac
-	fi
-    else
-	fps=`mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | tail -n 1`
-	if [ -n "$AC3" ]; then
-	    mkvextract tracks "$file" 1:"${title}".ac3 2:"${title}".264
-	    mplayer -ao pcm:file="${title}".wav:fast "${title}".ac3
-	    faac -o "${title}".aac "${title}".wav
-	elif [ -n "$AAC" ]; then
-	    mkvextract tracks "$file" 1:"${title}".264 2:"${title}".aac
-	else
-	    mkvextract tracks "$file" 1:"${title}".dts 2:"${title}".264
-	    ffmpeg -i "${title}".dts -acodec libfaac -ab 576k "${title}".aac
-	fi
+TMPDIR=/tmp/tmpfs
+
+function tracknum {
+    a=1
+    while [ -f "$2-$a.$1" ]; do
+	a=$(expr $a + 1)
+    done
+    return $a
+}
+
+function audio {
+    suf='aac'
+    if [ $2 = 'A_AC3' ]; then
+	mkvextract tracks "$file" $1:$TMPDIR/a.ac3 &> /dev/null
+	tracknum aac aud
+	n=$?
+	ffmpeg -i $TMPDIR/a.ac3 -strict experimental -acodec aac -ab 384k $TMPDIR/aud-$n.aac &> /dev/null
+#	mplayer -ao pcm:file=a.wav:fast a.ac3 
+#	faac -o "aud-$n.aac" a.wav
+       	rm $TMPDIR/a.ac3
+    elif [ $2 = 'A_AAC' ]; then
+	tracknum aac aud
+	n=$?
+	mkvextract tracks "$file" $1:$TMPDIR/aud-$n.aac &> /dev/null
+    elif [ $2 = 'A_MPEG/L3' ]; then
+	tracknum mp3 aud
+	n=$?
+	suf='mp3'
+	mkvextract tracks "$file" $1:$TMPDIR/aud-$n.mp3 &> /dev/null
     fi
-    mkvextract tracks Deadwood.S01E01.720p.BluRay.x264-CtrlHD.mkv 4:"${title}".srt
-    MP4Box -tmp /var/media/tmp -new "${directory}/${title}".mp4 -add "${title}".264 -add "${title}".aac -add "${title}".srt -fps $fps
-    rm -f "$title".aac "$title".dts "$title".ac3 "$title".264 "${title}".wav "${title}".srt
-  # if [ -f "${directory}/${title}".mp4 ]; then
-  # rm -f "$file"
-  # fi
+    tracks+=" -add $TMPDIR/aud-$n.$suf"
+}
+
+find . -type f -name '*.mkv' | while read file; do
+    echo "Converting $file"
+    directory=$(dirname "$file")
+    title=$(basename "$file" .mkv)
+    declare -a trackinfo=($(mkvinfo "$file" | egrep '(Track (type|number))|(Codec ID)' | gawk -F ':' '{print $2}'))
+    declare -a tracks
+    for i in $(seq 0 $(expr "${#trackinfo[@]}" / 3 - 1)); do 
+	num=$(expr $i '*' 3)
+	t=$(expr $i '+' 1)
+	ttype="${trackinfo[$num + 1]}"
+	codec="${trackinfo[$num + 2]}"
+	fps=$(mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | head -n 1)
+
+	if [ $ttype = 'audio' ]; then
+	    audio $t $codec
+	elif [ $ttype = 'subtitles' ]; then
+	    tracknum srt sub
+	    n=$?
+	    mkvextract tracks "$file" $t:$TMPDIR/sub-$n.srt &> /dev/null
+	    tracks+=" -add $TMPDIR/sub-$n.srt"
+	elif [ $ttype = 'video' ]; then
+	    tracknum 264 vid
+	    n=$?
+	    mkvextract tracks "$file" $t:$TMPDIR/vid-$n.264 &> /dev/null
+	    tracks+=" -add $TMPDIR/vid-$n.264"
+	fi
+
+    done
+    mp="MP4Box -tmp $TMPDIR -new \"$directory\"/\"$title\".m4v -fps $fps ${tracks[@]}"
+    echo "MP: $mp"
+    eval $mp &> /dev/null
+    echo "MP exit: $?"
+    r=$(echo "${tracks[@]}" | sed 's/-add //g')
+    echo "Remove: $r"
+    rm $r
+    # rm "file"
+    tracks=()
 done
-
-
-
-
-
