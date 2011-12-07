220202a820f968ea917830c801050fc5e8d22172
Justin Brown <justin.brown@fandingo.org>
2011-12-06 23:49:00 -0600

    Working for all test files
    Finally whipped Bash into shape. Working for all files, even with
    spaces.
    The only known bug is for files with "$" in them. They are interpreted
    as variables.
    
    The codec support is still a little thin:
    Audio:
    	AAC and MP3 are copied as is
    	AC3 is encoded to AAC
    Video:
    	264 is copied as is
    Need to add additional support for other codecs.

--- mkv-convert.sh
+++ mkv-convert.sh
@@ -1,40 +1,35 @@
 #!/bin/bash
 
 TMPDIR=/tmp/tmpfs
-
-function tracknum {
-    a=1
-    while [ -f "$2-$a.$1" ]; do
-	a=$(expr $a + 1)
-    done
-    return $a
-}
+n=1
 
 function audio {
     suf='aac'
     if [ $2 = 'A_AC3' ]; then
-	mkvextract tracks "$file" $1:$TMPDIR/a.ac3 &> /dev/null
-	tracknum aac aud
-	n=$?
-	ffmpeg -i $TMPDIR/a.ac3 -strict experimental -acodec aac -ab 384k $TMPDIR/aud-$n.aac &> /dev/null
+	mkvextract tracks "$file" $1:$TMPDIR/a.ac3 #~~!
+	while [ -f "$TMPDIR/$$-$n.$suf" ]; do
+	    n=$(expr $n + 1)
+	done
+	ffmpeg -i $TMPDIR/a.ac3 -strict experimental -acodec aac -ab 384k $TMPDIR/$$-$n.aac #~~!
 #	mplayer -ao pcm:file=a.wav:fast a.ac3 
 #	faac -o "aud-$n.aac" a.wav
        	rm $TMPDIR/a.ac3
     elif [ $2 = 'A_AAC' ]; then
-	tracknum aac aud
-	n=$?
-	mkvextract tracks "$file" $1:$TMPDIR/aud-$n.aac &> /dev/null
+	while [ -f "$TMPDIR/$$-$n.$suf" ]; do
+	    n=$(expr $n '+' 1)
+	done
+	mkvextract tracks "$file" $1:$TMPDIR/$$-$n.aac #~~!
     elif [ $2 = 'A_MPEG/L3' ]; then
-	tracknum mp3 aud
-	n=$?
 	suf='mp3'
-	mkvextract tracks "$file" $1:$TMPDIR/aud-$n.mp3 &> /dev/null
+	while [ -f "$TMPDIR/$$-$n.$suf" ]; do
+	    n=$(expr $n '+' 1)
+	done
+	mkvextract tracks "$file" $1:$TMPDIR/$$-$n.mp3 #~~!
     fi
-    tracks+=" -add $TMPDIR/aud-$n.$suf"
+    tracks+=" -add $TMPDIR/$$-$n.$suf"
 }
 
-find . -type f -name '*.mkv' | while read file; do
-    echo "Converting $file"
+function convertmkv {
     directory=$(dirname "$file")
     title=$(basename "$file" .mkv)
     declare -a trackinfo=($(mkvinfo "$file" | egrep '(Track (type|number))|(Codec ID)' | gawk -F ':' '{print $2}'))
@@ -47,27 +42,46 @@ find . -type f -name '*.mkv' | while read file; do
 	fps=$(mkvinfo "$file" | grep duration | sed 's/.*(//' | sed 's/f.*//' | head -n 1)
 
 	if [ $ttype = 'audio' ]; then
+	    echo -e "\n\nstarting audio\n\n"
 	    audio $t $codec
 	elif [ $ttype = 'subtitles' ]; then
-	    tracknum srt sub
-	    n=$?
-	    mkvextract tracks "$file" $t:$TMPDIR/sub-$n.srt &> /dev/null
-	    tracks+=" -add $TMPDIR/sub-$n.srt"
+	    suf="srt"
+	    while [ -f "$TMPDIR/$$-$n.$suf" ]; do
+		n=$(expr $n '+' 1)
+	    done	
+	    echo -e "\n\nStarting subtitles\n\n"
+	    mkvextract tracks "$file" $t:$TMPDIR/$$-$n.srt #~~!
+	    tracks+=" -add $TMPDIR/$$-$n.srt"
 	elif [ $ttype = 'video' ]; then
-	    tracknum 264 vid
-	    n=$?
-	    mkvextract tracks "$file" $t:$TMPDIR/vid-$n.264 &> /dev/null
-	    tracks+=" -add $TMPDIR/vid-$n.264"
+	    suf="264"
+	    while [ -f "$TMPDIR$$-$n.$suf" ]; do
+		n=$(expr $n '+' 1)
+	    done
+	    echo -e "\n\nStarting video\n\n"
+	    mkvextract tracks "$file" $t:$TMPDIR/$$-$n.264 #~~!
+	    tracks+=" -add $TMPDIR/$$-$n.264"
 	fi
 
     done
-    mp="MP4Box -tmp $TMPDIR -new \"$directory\"/\"$title\".m4v -fps $fps ${tracks[@]}"
-    echo "MP: $mp"
-    eval $mp &> /dev/null
-    echo "MP exit: $?"
+    echo -e "Temp files\n$(ls $TMPDIR/)"
+    mp="MP4Box -tmp $TMPDIR -new \"$directory/$title.m4v\" -fps $fps ${tracks[@]}"
+    echo -e "\n\nMP4Box $mp\n\n"
+    sleep 10
+    eval $mp #~~!1
     r=$(echo "${tracks[@]}" | sed 's/-add //g')
-    echo "Remove: $r"
     rm $r
     # rm "file"
-    tracks=()
-done
+    echo "$directory/$title.m4v" >> /tmp/conversions.log
+}
+
+file="$@"
+if [ ! -f "$file" ]; then
+    echo "Invalid file: $file"
+    exit 1
+fi
+convertmkv
+if [ -f "$directory/$title.m4v" ]; then
+    echo "Converted $title"
+else
+    echo "Failed to convert $title"
+fi
